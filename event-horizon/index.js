2294f21c36085ca5777676db5160ba23fada57ef
Edgar U. Joya <eujc21@gmail.com>
2016-09-27 00:16:10 -0400

    global and window check

--- event-horizon/index.js
+++ event-horizon/index.js
@@ -1,7 +1,14 @@
 import EventHorizon from './event-horizon';
 import Trackr from 'trackr';
 
-// react komposer need tracker so we need to make it global
-global.Tracker = Trackr;
+if(global) {
+  // react komposer need tracker on native so we need to make it global
+  console.log(global);
+  global.Tracker = Trackr;
+} else if(window) {
+  // react komposer need tracker on browser so we need to assign it to the window
+  console.log(window);
+  window.Tracker = Trackr;
+}
 
 export default new EventHorizon();
